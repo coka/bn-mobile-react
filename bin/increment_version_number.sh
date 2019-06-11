543bd6f6b0e6d9b5d29fc6d99c95e46fc8cda627
Krakaw <41575888+Krakaw@users.noreply.github.com>
2019-06-11 12:27:50 +0200

    Added check for previous travis increments

--- bin/increment_version_number.sh
+++ bin/increment_version_number.sh
@@ -1,6 +1,16 @@
 #! /usr/bin/env bash
 set -e # exit entire script when command exits with non-zero status
 
+#If the last commit was a version bump, don't bump again
+
+LAST_COMMIT=`git log -1 --pretty=%B`
+if [[ $LAST_COMMIT == *"Travis build:"* ]];
+    #The last commit was a build, do not run again, but it is not a failure
+    exit 0
+fi
+
+
+
 git config --global user.email "travis@travis-ci.org"
 
 git config --global user.name "Travis CI"
